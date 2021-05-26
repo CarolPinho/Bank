c3a081e46bf54b249e6db5f8bc0b562cb584e660
CarolPinho <caroolpinho@hotmail.com>
2021-05-26 16:17:57 -0300

    Projeto Bank

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta MinhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Carolina Pinho");
+
+            Console.WriteLine(MinhaConta.ToString());
+        }
+    }
+}
